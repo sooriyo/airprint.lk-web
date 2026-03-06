24eafd5be13a117425040b20d4da4884b8c4f747
Tharuka Soooriyaarachchi <tharukaravisara@gmail.com>
2026-03-06 18:24:58 +0530

    chore: ignore eslint and typescript errors during build

--- next.config.js
+++ next.config.js
@@ -18,6 +18,12 @@ const nextConfig = {
       }),
     ],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   reactStrictMode: true,
   redirects,
   webpack: (webpackConfig) => {
